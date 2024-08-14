e1e711aac184bd56aed0db14b50d382f731c0083
SymTrkl <not.ink@gmail.com>
2024-08-14 17:12:53 -0600

    made python script to generate meta.json so i don't have to use emoji-gen

--- pack.sh
+++ pack.sh
@@ -10,19 +10,36 @@ else
 	emojidir=webres/$1
 	zipfile=releases/$1.zip
 	tarfile=releases/$1.tar.gz
+	jsonfile=webres/$1/meta.json
 	# check if dir exists
 	if [ ! -d $emojidir ]; then
 		echo "ERROR: Directory '$1' does not exist."
-	#check if dir is empty (apparently read NEEDS a variable :/ )
+	# check if dir is empty (apparently read NEEDS a variable :/ )
 	elif find $emojidir -mindepth 1 -maxdepth 1 | read ugh; then
-		# if .zip exists, remove it so kainoa's tool doesn't choke
+		# remove old files
 		if [ -f $zipfile ]; then
 			rm $zipfile
 		fi
-		emoji-gen local --folder $emojidir --output $zipfile
+		if [ -f $tarfile ]; then
+			rm $tarfile
+		fi
+		if [ -f $jsonfile ]; then
+			rm $jsonfile
+		fi
+		# generate .tar.gz for masto/akkoma
 		tar -czf $tarfile -C webres $1
 		if [ -f $tarfile ]; then
 			echo "Created '$tarfile' sucessfully!"
+		fi
+		# generate meta.json
+		python3 pack_json.py $1
+		if [ -f $jsonfile ]; then
+			echo "Created '$jsonfile' sucessfully!"
+		fi
+		# generate .zip for misskey
+		zip -jq $zipfile $emojidir/*
+		if [ -f $zipfile ]; then
+			echo "Created '$zipfile' sucessfully!"
 		fi
 	else
 		echo "ERROR: Directory '$1' is empty."
---
+++ pack_json.py
@@ -0,0 +1,34 @@
+# misskey-compatible meta.json generator
+#
+# IMPORTANT!
+# this script is meant to be called from pack.sh!
+# it is assumed that the passed value has already been sanity-checked!
+# subdirectories in the emoji directory will probably break this
+
+import json
+import sys
+import os
+
+folder = sys.argv[1]
+relpath = "webres/" + folder # edit this to use this script in another environment
+relfile = relpath + "/meta.json"
+
+emojis = []
+
+for filename in os.listdir(relpath):
+	if filename != "meta.json":
+		emojis.append({
+			"downloaded": True,
+			"filename":  filename,
+			"emoji": {
+				"name": filename.rsplit(".")[0],
+				"category":folder.capitalize(),
+				"aliases": []
+			}
+		})
+
+output = {"emojis": emojis}
+
+outfile = open(relfile, "w")
+outfile.write(json.dumps(output))
+outfile.close()
