908ffeb21c9145b304b7681a1da6252b87ba9fc7
lauren n. liberda <lauren@selfisekai.rocks>
2024-09-08 19:54:19 +0200

    compress all webres emojis with zopfli
    filesize savings by 20-35% on most files

--- webgen.sh
+++ webgen.sh
@@ -5,6 +5,7 @@ for f in $(find highres/ -type f -name "*.png"); do
 	emojipath="$(echo $f | sed -e 's/highres/webres/;s/_1200\|_2048//')"
 	if [ ! -f $emojipath ]; then
 		convert -scale x256 "$f" "$emojipath"
+		oxipng --zopfli -o max "$emojipath"
 		echo "Downscaled '$f' to '$emojipath'"
 	fi
 done
